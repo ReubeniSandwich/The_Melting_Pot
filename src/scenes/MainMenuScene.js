ad7c55d37ea1b181451078af1e2fdfe188e224f3
Reuben Sanchez <reubenisanchez@gmail.com>
2020-12-10 21:28:42 -0500

    do not put begin game into a function apparently

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -17,16 +17,13 @@ export default class MainMenuScene extends Phaser.Scene {
 
 
 
-        beginGame();
-
-          const beginGame = () => {
             this.add.text(20, 20, "Loading game...");
             setTimeout(() => {
                 // important that they start in conjunction... I think... for passing data using event
                 this.scene.start('KitchenScene');
                 this.scene.launch('FridgeScene')
               }, 1000);
-          }
+          
       }
 
     update () {
