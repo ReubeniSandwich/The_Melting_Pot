81bc38887e2a5cc6438f277bcc0dd6f7076d6d85
Reuben Sanchez <reubenisanchez@gmail.com>
2020-12-12 23:45:07 -0500

    refactored loading scene

--- src/scenes/LoadingScene.js
+++ src/scenes/LoadingScene.js
@@ -7,8 +7,8 @@ export default class LoadingScene extends Phaser.Scene {
         super("LoadingScene");
     }
 
-    init () {
-        
+    init() {
+
     }
 
     preload() {
@@ -18,29 +18,19 @@ export default class LoadingScene extends Phaser.Scene {
 
     create() {
         const logo = this.add.image(400, 200, "logo").setScale(.3);
-        let startButton = this.add.image(400, 500, "startButton").setScale(.4, .3);
+        let startButton = this.add.image(400, 500, "startButton").setScale(.4, .3).setInteractive();
         // this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#FFFFFF");
-        startButton.setInteractive();
 
         startButton.once("pointerdown", function () {
             this.cameras.main.fadeOut(1000, 0, 0, 0)
-
             this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
                 this.scene.start('MainMenuScene');
-            }); 
-            // this.scene.start('KitchenScene');
-            // this.scene.launch('FridgeScene');
+            });
         }, this);
-        
-        // this.add.text(20, 20, "Loading game...");
-        // setTimeout(() => {
-        //     // important that they start in conjunction... I think... for passing data using event
-        //     this.scene.start('KitchenScene');
-        //     this.scene.launch('FridgeScene')
-        //   }, 3000);
-      }
 
-    update () {
+    }
+
+    update() {
 
     }
 }
